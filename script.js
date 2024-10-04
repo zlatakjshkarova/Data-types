20d26a361d31e31a75a4a8ac041e9d0428ec34ac
zlatakjshkarova <147971125+zlatakjshkarova@users.noreply.github.com>
2024-10-04 23:48:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+let x = 5;
+
+console.log(typeof (x));
+x = Number(x);
+let y = "hello";
+y = String(y);
+console.log(typeof (y));
+
+let z = true;
+z = Boolean(z);
+console.log(`z=${z}`);
+
+
+let w = null;
+w = Boolean(w);
+console.log(`w=${w}`)
+
+let v;
+v = String(v);
+console.log(typeof (v));
+
+let a = "10";
+console.log(Number(a));
+let m = "20.5";
+console.log(parseInt(a));
+let e = '30px';
+console.log(parseFloat(e));
+let r = "10.5";
+r = parseFloat(r);
+console.log(r)
+let q = "20";
+q = Number(q);
+console.log(q);
+
+let f = "30.25px";
+f = parseFloat(f);
+console.log(f);
+
+
+let userName = " Джон";
+let age = '30';
+console.log(`'Меня зовут${userName} , мне ${age} лет'`);
+
+
+alert('«Я изучаю JavaScript»');
+
+
+let num = 123;
+alert(num);
+let d = 1 + 2 + 3;
+d = Number(d);
+alert(d);
+
+
+let ds = 6;
+ds = Number(ds);
+df = 3;
+df = Number(df);
+let sds = df; ds;
+console.log(sds);
+sds = +prompt(ds - df);
+sds = +prompt(ds * df);
+sds = +prompt(ds / df);
+
+
